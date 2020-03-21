8d42a65f0382b92aa8e72c11e5c3af71b34a66a8
Alan Wong <alan.wong@ad.prizm.digital>
2020-03-22 00:15:13 +0800

    update paths

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,15 @@
 # Source functions
 sapply(list.files("./src", full.names = TRUE), source)
 
-# Load Data
+# Define paths
 url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 target.folder = "./data"
 data.folder <- file.path(target.folder, "UCI HAR Dataset")
+output.folder <- "./output"
+output.file.path <- file.path(output.folder, "tidy_dataset.txt")
 
+
+# Load Data
 df <- get_full_data(url, target.folder, data.folder)
 
 # Filter columns
@@ -13,3 +17,5 @@ df <- filter_columns(df)
 
 # Summarize data
 summary.df <- summarize_data(df)
+
+# Write to output
